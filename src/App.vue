562542145cfc59fdd2823953af5383059fdfc820
yangwenlong <81355146@qq.com>
2020-08-06 20:25:44 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="header">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header{
+  width: 1100px;
+
+}
+</style>
